f28171e9fdcab2e31dfab7951f8fe5a4ed979194
yeop <woco11@naver.com>
2024-02-29 20:45:37 +0900

    작은 블로그 데모

--- routes/blog.js
+++ routes/blog.js
@@ -49,7 +49,53 @@ router.get("/posts/:id", async function(req, res){
         return res.status(404).render("404");
     }
 
-    res.render("post-detail",{ post:posts[0] });
+    const postData = {
+        ...posts[0], //단일 게시물의 모든데이터를 해당 객체로 가져온다.
+        date : posts[0].date.toISOString(), //Mysql 패키지에 의해 자바스크립트 객체로 변환
+        humanReadableDate : posts[0].date.toLocaleDateString("en-US",{
+            weekday : "long",
+            year : "numeric",
+            month : "long",
+            day : "numeric"
+        }) //다른 내장 메소드인 날짜 객체를 호출할수있음
+    };
+
+    res.render("post-detail",{ post:postData });
+});
+
+router.get("/posts/:id/edit", async function(req, res){ //업데이트전 페이지에 보여줌
+    const query =`
+    SELECT * FROM posts WHERE id = ?
+    `;
+    const [posts] = await db.query(query,[req.params.id]);
+
+    if (!posts || posts.length === 0 ){ //요청 주소가 잘못된경우 404오류 
+        return res.status(404).render("404");
+    }
+
+    res.render("update-post", { post: posts[0]});
+});
+
+router.post("/posts/:id/edit", async function(req, res){ //업데이트 하기
+    const query = `
+    UPDATE posts SET title = ?, summary = ? , body = ?
+    WHERE id = ? 
+    `;
+
+    await db.query(query, [
+        req.body.title,
+        req.body.summary,
+        req.body.content,
+        req.params.id
+    ]); // 4개의 ? 이기때문에 순서에 맞춰서 작성
+
+    res.redirect("/posts")
+});
+
+router.post("/posts/:id/delete", async function(req, res){ //게시물 삭제
+    await db.query("DELETE FROM posts WHERE id = ? ", [req.params.id]);
+    
+    res.redirect("/posts"); 
 });
 
 module.exports = router;
